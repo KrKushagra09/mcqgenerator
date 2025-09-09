f1b2c377104a9ba9a888a7c3620ce31d4930ff09
Kushagra <kumarkushagra09@gmail.com>
2025-09-09 13:51:22 +0530

    made the complete file of jupy notebook aling with csv creation

--- setup.py
+++ setup.py
@@ -1,1 +1,24 @@
 #for installing local packages in virtual environment
+from setuptools import find_packages, setup
+
+setup(
+    name="mcqgenerator",
+    version="0.0.1",
+    author="Kumar Kushagra",
+    author_email="kumarkushagra09@gmail.com",
+    description="A package for generating MCQs using OpenAI, LangChain, Hugging Face, and Streamlit",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "langchain",
+        "streamlit",
+        "python-dotenv",
+        "PyPDF2",
+        "huggingface_hub",
+        "torch",
+        "torchvision",
+        "torchaudio",
+        "transformers",
+        "tensorflow"
+    ],
+)
